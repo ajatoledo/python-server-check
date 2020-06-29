af3bfe4bb5204ff24382943277e875673e2ce49b
ajatoledo <aaguilar@thechicagoschool.edu>
2020-06-28 23:32:50 -0400

    updated to use pushover

--- README.md
+++ README.md
@@ -4,19 +4,28 @@ A simple Python script to see if a server is still alive. If the
 server does not respond after a specified number of attempts (default: 10), the script will email
 recipients indicated in the script.
 
+## Updated to Use Pushover
+
 ## Requirements
 
-The `python-server-check` program requires Python and *some* email daemon is installed.  On Debian-based systems, [Exim4](https://wiki.debian.org/Exim) is a reasonable choice.  It can be installed and configured using the following commands.
+<!-- The `python-server-check` program requires Python and *some* email daemon is installed.  On Debian-based systems, [Exim4](https://wiki.debian.org/Exim) is a reasonable choice.  It can be installed and configured using the following commands.
 
 ```
 sudo apt-get install exim4
 sudo dpkg-reconfigure exim4-config
 ```
+ -->
+
+The `python-server-check` program requires Python and a pre-configured [Pushover](https://pushover.net) application with user and application token.  
 
 ## Building
 
-No need to build anything.  However, you will want to modify the
+<!-- No need to build anything.  However, you will want to modify the
 script to send email to the appropriate people.  After modifications,
+just run using the Python interpreter. -->
+
+No need to build anything.  However, you will want to modify the
+script to read to a config file, or alternatively input user and app tokens each time teh script is run.  After modifications,
 just run using the Python interpreter.
 
 ## Basic Usage
@@ -29,8 +38,8 @@ usage: checkServer.py [-h] [-a ATTEMPTS] [-w WAIT] [-g]
 
 positional arguments:
   host                  host name to verify
-  sender                email sender
-  recipients            email recipients (default: sender)
+  token 	            pushover application token
+  user		            pushover user token
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -43,19 +52,19 @@ optional arguments:
 As an example, invoking the script as
 
 ```bash
-./checkServer.py foo.com a@email.com b@email.com
+./checkServer.py foo.com -t app_token -u user_token
 ```
 
 checks to see if `foo.com` reachable by ping.  If `foo.com` is not
-reachable, an email is sent from `a@email.com` to `b@email.com`.
-
-If no email recipients are specified, an email is sent to the sender
-email address.
+reachable, a call to the specified Pushover app is made.
 
 Using the `--wget` option allows you to specify an alternative port if
 icmp traffic is blocked or ignored.  Specify the port by appending it
 to the host name.  For example, specify `foo.com:8080` would use wget
 to check for the default page served on port **8080**.
+
+## Config Setting
+If you want the script to read a pre-configured config file, create a file called `config` using the supplied `config_example.json` and place it into the same directory ad the python script. Using this allows you to omit application and user token when running the script.
 
 ## Using Cron
 
@@ -72,7 +81,7 @@ at a terminal will allow you to edit the local user's cron job file.
 Adding a line such as
 
 ```
-*/30 * * * * ~/checkServer.py foo.com a@email.com b@email.com
+*/30 * * * * ~/checkServer.py -t app_token -u user_token
 ```
 
 will check every 30 minutes that the server is still accessible.  This
@@ -83,8 +92,3 @@ is executable.
 
 The project is licensed under the terms of the
 [GPL3](https://www.gnu.org/licenses/gpl-3.0.en.html) license.
-
-<!--  LocalWords:  sendmail checkServer py wget icmp Cron cron
- -->
-<!--  LocalWords:  crontab
- -->
--- checkServer.py
+++ checkServer.py
@@ -4,41 +4,42 @@
 
 import argparse
 import os
+import sys
+import json
 import time
-import smtplib
-
-from email.mime.text import MIMEText
+import http.client
+import urllib
 
 def ping(hostname, useWget):
-    print "Attempting to ping host %s..." % hostname
+    print('Attempting to ping host %s...' % hostname)
     if useWget:
         # This option is if icmp is blocked
-        return os.system("wget -nv -O - " + hostname  + " > /dev/null 2>&1")
+        return os.system('wget -nv -O - ' + hostname  + ' > /dev/null 2>&1')
     else:
         # Send one packet and wait up to 10 seconds for a response
-        return os.system("ping -c 1 -W 10 " + hostname  + " > /dev/null")
+        return os.system('ping -c 1 -W 10 ' + hostname  + ' > /dev/null')
 
 
 def main():
 
+    # read in config file
+    with open(os.path.join(sys.path[0], 'config')) as f:
+        data = json.load(f)
+
     parser = argparse.ArgumentParser()
     parser.add_argument('host', help='host name to verify')
-    parser.add_argument('sender', help='email sender')
-    parser.add_argument('recipients', nargs='*', help='email recipients (default: sender)')
+    parser.add_argument('-t', '--token', default=data['pushover']['token'], help='pushover application token')
+    parser.add_argument('-u', '--user', default=data['pushover']['user'], help='pushover user token')
     parser.add_argument('-a', '--attempts', type=int, default=10, help='max attempts')
     parser.add_argument('-w', '--wait', type=int, default=20, help='wait time in seconds (default: 20)')
-    parser.add_argument('-g', '--wget', action="store_true", help='use wget instead of icmp ping')
+    parser.add_argument('-g', '--wget', action='store_true', help='use wget instead of icmp ping')
 
     try:
         args = parser.parse_args()
 
         hostname = args.host
-        sender = args.sender
-        recSet = set(args.recipients)
-        # Only add sender as recipient if no sender specified
-        if len(recSet) == 0:
-            recSet.add(args.sender)
-        receivers = ','.join(recSet)
+        token = args.token
+        user = args.user
         maxAttempts = args.attempts
         waitTime = args.wait # seconds
         useWget = args.wget
@@ -51,26 +52,29 @@ def main():
     attempts = 1
 
     while response != 0 and attempts < maxAttempts:
-        print "Attempt %d to ping host %s failed. Trying again in %d seconds." % (attempts, hostname, waitTime)
+        print('Attempt %d to ping host %s failed. Trying again in %d seconds.' % (attempts, hostname, waitTime))
         time.sleep(waitTime)
         response = ping(hostname, useWget)
         attempts+=1
 
     if response != 0:
-        print "Attempt %d to ping host %s failed. Giving up and sending email." % (attempts, hostname)
-        msg = MIMEText("%s failed to respond after %d ping attempts.  Someone should probably investigate." % (hostname, attempts))
-        msg['Subject'] = "Host %s is unresponsive." % hostname
-        msg['From'] = sender
-        msg['To'] = receivers
+        print('Attempt %d to ping host %s failed. Giving up and sending pushover alert.' % (attempts, hostname))
+        msg = '%s failed to respond after %d ping attempts. Someone should probably investigate.' % (hostname, attempts)
+        
+        conn = http.client.HTTPSConnection('api.pushover.net:443')
+        conn.request('POST', '/1/messages.json',
+            urllib.parse.urlencode({
+                'token': token,
+                'user': user,
+                'message': msg,
+            }), { 'Content-type': 'application/x-www-form-urlencoded' })
+        conn.getresponse()
 
-        s = smtplib.SMTP("127.0.0.1")
-        s.sendmail(sender, msg["To"].split(","), msg.as_string())
-        s.quit()
-
-        print "Failed to ping %s.  Sent email to %s." % (hostname, receivers)
+        # print 'Failed to ping %s.  Sent email to %s.' % (hostname, receivers)
+        print('Failed to ping %s.  Sent pushover alert.' % (hostname))
 
     else:
-        print "Successful ping response from %s.  It's alive!" % hostname
+        print('Successful ping response from %s.  It\'s alive!' % (hostname))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
